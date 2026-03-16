eb16f11eedb0f169061bdfc777713b0dfe635d6c
Haha999777 <adam941211@gmail.com>
2026-03-16 18:58:41 +0800

    feat: add cardview

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            CardView()
+            CardView(isFaceUp: false)
             CardView()
             CardView()
             CardView()
